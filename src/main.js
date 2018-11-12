f08247826261eb1f43cd836f54d4126a186ac163
Caue Prado <caue.prado0@gmail.com>
2018-11-12 01:48:20 -0200

    Adicionado o pacote bootstrap-vue.

--- src/main.js
+++ src/main.js
@@ -1,17 +1,25 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import Buefy from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+
+Vue.use(Buefy)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   store,
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
