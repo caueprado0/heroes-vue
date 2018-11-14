ac75650bef360f87bc08ffa2bf1cad2aafb017af
Caue Prado <caue.prado0@gmail.com>
2018-11-14 12:26:02 -0200

    Reconfigurado o webpack.base.

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -7,10 +7,7 @@ function resolve(dir) {
   return path.join(__dirname, '..', dir)
 }
 
-
-
 module.exports = {
-  context: path.resolve(__dirname, '../'),
   entry: {
     app: './src/main.js'
   },
@@ -24,7 +21,7 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
+      '@': resolve('src')
     }
   },
   module: {
@@ -36,14 +33,7 @@ module.exports = {
     {
       test: /\.js$/,
       loader: 'babel-loader',
-      include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
-    },
-    {
-      test: /\.css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-      ]
+      include: [resolve('src'), resolve('test')]
     },
     {
       test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
@@ -70,17 +60,5 @@ module.exports = {
       }
     }
     ]
-  },
-  node: {
-    // prevent webpack from injecting useless setImmediate polyfill because Vue
-    // source contains it (although only uses it if it's native).
-    setImmediate: false,
-    // prevent webpack from injecting mocks to Node native modules
-    // that does not make sense for the client
-    dgram: 'empty',
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty',
-    child_process: 'empty'
   }
 }
