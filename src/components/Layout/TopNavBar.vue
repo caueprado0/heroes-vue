deb890e5680f11429c7b62825e60c001dc3cbdd8
Caue Prado <caue.prado0@gmail.com>
2018-11-14 12:26:29 -0200

    Criado o menu superior.

---
+++ src/components/Layout/TopNavBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-navbar toggleable="md" type="dark" variant="dark">
+  <b-navbar-brand href="">Heroes VUE</b-navbar-brand>
+  <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+  <b-collapse is-nav id="nav_collapse">
+    <!-- Right aligned nav items -->
+    <b-navbar-nav class="ml-auto">
+      <b-nav-item href="#">Login</b-nav-item>
+    </b-navbar-nav>
+  </b-collapse>
+</b-navbar>
+</template>
+
+<script>
+import 'font-awesome/css/font-awesome.css'
+
+export default {
+  name: 'TopNavBar'
+}
+</script>
+
+<style>
+
+</style>
