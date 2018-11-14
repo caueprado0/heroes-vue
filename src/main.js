270cace0e9a0050007fa2574f1f8773ca2437b45
Caue Prado <caue.prado0@gmail.com>
2018-11-14 12:27:10 -0200

    Configurado main

--- src/main.js
+++ src/main.js
@@ -1,17 +1,16 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import Buefy from 'bootstrap-vue'
+import BootstrapVue from 'bootstrap-vue'
+
+import App from './App.vue'
+import router from './router/index'
+import store from './store'
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-import App from './App.vue'
-import router from './router'
-import store from './store'
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
-
-Vue.use(Buefy)
 
 /* eslint-disable no-new */
 new Vue({
