cd123ade22b9fd80a07c6efd5b0711c04dbc1b24
Caue Prado <caue.prado0@gmail.com>
2018-11-14 13:58:18 -0200

    Adicionado novas rotas.

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,4 +4,24 @@ export default [{
   path: '/',
   name: 'Login',
   component: Login
-}]
+},
+{
+  path: '/personagens',
+  name: 'Personagens',
+  component: Login
+}, {
+  path: '/favoritos',
+  name: 'Favoritos',
+  component: Login
+},
+{
+  path: '/logout',
+  name: 'Logout',
+  component: Login
+},
+{
+  path: '/404',
+  name: '404',
+  component: Login
+},
+]
