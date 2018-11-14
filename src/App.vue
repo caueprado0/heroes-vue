0c174711cf80ef10fb8ded75a144b574ea8a5357
Caue Prado <caue.prado0@gmail.com>
2018-11-14 13:50:44 -0200

    App vue finalizada.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
+  <div id="main">
     <TopNavBar></TopNavBar>
-    <router-view></router-view>
+    <b-container>
+      <router-view></router-view>
+    </b-container>
   </div>
 </template>
 
@@ -16,6 +18,12 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+#main {
+  height: 100vh;
+}
+.container {
+  min-height: 100%;
+  height: 100%;
+}
 </style>
