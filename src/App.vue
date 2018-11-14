2abe2f1eb70328f71dd2864a36bd8d069c010acc
Caue Prado <caue.prado0@gmail.com>
2018-11-14 12:26:17 -0200

    App vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div>
+    <TopNavBar></TopNavBar>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+import TopNavBar from './components/Layout/TopNavBar.vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    TopNavBar
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
