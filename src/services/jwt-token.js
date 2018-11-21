512ebddd8285d4a3bdf6f80e788863335d87d8c8
Caue Prado <caue.prado0@gmail.com>
2018-11-21 09:31:32 -0200

    Serviço para lidar com o JWT.

---
+++ src/services/jwt-token.js
@@ -0,0 +1,40 @@
+import _ from 'lodash'
+import SessionStorage from './session-storage'
+import Login from './login'
+
+export default {
+  get token() {
+    const tokenObject = SessionStorage.getObject('token')
+    if (_.has(tokenObject, 'access_token') && !_.isEmpty(tokenObject.access_token)) {
+      return tokenObject.access_token
+    }
+    return null
+  },
+
+  set token(value) {
+    return SessionStorage.setObject('token', value)
+  },
+
+  get tokenType() {
+    const tokenObject = SessionStorage.getObject('token')
+    if (_.has(tokenObject, 'token_type') && !_.isEmpty(tokenObject.token_type)) {
+      return tokenObject.token_type
+    }
+    return 'Bearer'
+  },
+
+  login(email, password) {
+    return Login(email, password)
+      .then((response) => {
+        if (_.has(response, 'data.data') && _.isObject(response.data.data)) {
+          this.token = response.data.data
+          return this.getAuthorizationHeader()
+        }
+        return null
+      })
+  },
+
+  getAuthorizationHeader() {
+    return `${this.tokenType} ${this.token}`
+  }
+}
