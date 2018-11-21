4ea2a354d2b9c61f46400bfcc4d8bd725381f73c
Caue Prado <caue.prado0@gmail.com>
2018-11-21 09:45:07 -0200

    Coloquei um middleware no router.

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
 import routes from './routes'
-
+import store from '../store'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   mode: 'history',
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (!store.state.auth.logged && to.meta.auth) {
+    return router.push({
+      name: 'Login'
+    })
+  }
+  return next()
 })
 
 export default router
