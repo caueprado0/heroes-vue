f03ba003d3377ec950559c03d10ba6912b8b0f1d
Caue Prado <caue.prado0@gmail.com>
2018-11-21 15:50:17 -0200

    métodos de personagens.

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import _ from 'lodash'
 import Jwt from '../services/jwt-token'
-
+import Personagens from '../services/personagens'
 
 Vue.use(Vuex)
 
@@ -22,12 +22,33 @@ const mutations = {
       .catch(() => {
         state.auth.logged = false
       })
+  },
+  personagens(state, data) {
+    state.personagens.personagens = data
+  },
+
+  personagensOffset(state, data) {
+    state.personagens.offset = data
+  },
+
+  personagensLimit(state, data) {
+    state.personagens.limit = data
+  },
+
+  personagensTotal(state, data) {
+    state.personagens.total = data
   }
 }
 
 const state = {
   auth: {
     logged: !_.isNull(Jwt.token)
+  },
+  personagens: {
+    personagens: [],
+    offset: 0,
+    limit: null,
+    total: null
   }
 }
 
@@ -43,7 +64,40 @@ const actions = {
         }
         return response
       })
+  },
+  personagens(context, params = {}) {
+    Personagens.all(params)
+      .then((response) => {
+        const data = _.has(response, 'results') && _.isArray(response.results) ? response.results : []
+        context.commit('personagens', data)
+        return response
+      })
+      .then((response) => {
+        const data = _.has(response, 'offset') && _.isNumber(response.offset) ? response.offset : null
+        context.commit('personagensOffset', data)
+        return response
+      })
+      .then((response) => {
+        const data = _.has(response, 'limit') && _.isNumber(response.limit) ? response.limit : null
+        context.commit('personagensLimit', data)
+        return response
+      })
+      .then((response) => {
+        const data = _.has(response, 'total') && _.isNumber(response.total) ? response.total : null
+        context.commit('personagensTotal', data)
+        return response
+      })
+  },
+
+  personagensShow(context, {
+    id
+  }) {
+    Personagens.id(id)
+      .then((response) => {
+        context.commit('personagens', response)
+      })
   }
+
 }
 
 
