14c0f7b6d578b07ec368ac3b9390b589f2f73016
Caue Prado <caue.prado0@gmail.com>
2018-11-21 17:11:58 -0200

    serviço de favoritos criado.

---
+++ src/services/favoritos.js
@@ -0,0 +1,57 @@
+import axios from 'axios'
+import _ from 'lodash'
+import interceptorsSetup from './interceptors'
+
+interceptorsSetup()
+
+export default class Favoritos {
+  static create(id) {
+    return axios({
+      method: 'POST',
+      url: `http://localhost:8006/api/v1/favoritos/${id}`,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => response.data.data)
+  }
+
+  static all(queryParams) {
+    return axios({
+      method: 'GET',
+      url: `http://localhost:8006/api/v1/personagem${this.getQueryParams(queryParams)}`,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => response.data.data)
+  }
+
+  static id(id, queryParams) {
+    return axios({
+      method: 'GET',
+      url: `http://localhost:8006/api/v1/personagem/${id}${this.getQueryParams(queryParams)}`,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => {
+        console.log(response.data.data)
+        return response.data
+      })
+  }
+
+  static getQueryParams(queryParams) {
+    if (_.isObject(queryParams) && _.size(_.keys(queryParams)) > 0) {
+      let query = '?'
+      _.forEach(queryParams, (value, key) => {
+        query = query.concat(`${key}=${value}&`)
+      })
+      return query.slice(0, -1)
+    }
+    return ''
+  }
+}
